3abe7df88f03253dcf377bac706843a1967d1c39
ferdinand hubbard <ferdinandhubbard@gmail.com>
2021-05-25 23:49:26 +0100

    linked other matrix operations to backend

--- GUI/operation-handler.cpp
+++ GUI/operation-handler.cpp
@@ -4,27 +4,111 @@
 #include "../matrix.cpp"
 string DoOperation(string inputstring)
 {
+  bool resultismatrix = true;
   string outputstring;
   string optype;
-  int mat1rows, mat1cols, mat2rows, mat2cols;
-  double** mat1;
-  double** mat2;
+  int matArows, matAcols, matBrows, matBcols;
+  double** matA;
+  double** matB;
   int* resultorder = new int[2];
   double** result;
+  double constant;
 
-  ParseStringTo2dArray(optype, mat1rows, mat1cols, mat2rows, mat2cols, mat1, mat2, inputstring);
+  ParseStringTo2dArray(optype, matArows, matAcols, matBrows, matBcols, matA, matB, inputstring);
+
+  if (optype == "Add")
+  {
+
+  }
+
+  if (optype == "Sub")
+  {
+
+  }
 
   if (optype == "Mult")
   {
 
-    result = multiplymatrices(mat1, mat2, mat1rows, mat1cols, mat2rows, mat2cols);
-    resultorder[0] = mat1rows;
-    resultorder[1] = mat2cols;
+    result = multiplymatrices(matA, matB, matArows, matAcols, matBrows, matBcols);
+    resultorder[0] = matArows;
+    resultorder[1] = matBcols;
     //printmatrix(result, 2, 2);
 
   }
 
-  outputstring = ConvertMatToString(result, resultorder);
+
+  if (optype == "DetA")
+  {
+    outputstring = finddeterminant(matA, matArows, matAcols);
+    resultismatrix = false;
+  }
+
+  if (optype == "DetA")
+  {
+    outputstring = finddeterminant(matB, matBrows, matBcols);
+    resultismatrix = false;
+  }
+
+  if (optype == "InvA")
+  {
+    result = inversematrix(matA, matArows, matAcols);
+    resultorder[0] = matArows;
+    resultorder[1] = matAcols;
+  }
+
+  if (optype == "InvB")
+  {
+    result = inversematrix(matB, matBrows, matBcols);
+    resultorder[0] = matBrows;
+    resultorder[1] = matBcols;
+  }
+
+  if (optype == "TransA")
+  {
+    result = transposematrix(matA, matArows, matAcols);
+    resultorder[0] = matArows;
+    resultorder[1] = matAcols;
+  }
+
+  if (optype == "TransB")
+  {
+    result = transposematrix(matB, matBrows, matBcols);
+    resultorder[0] = matBrows;
+    resultorder[1] = matBcols;
+  }
+
+  if (optype == "SquareA")
+  {
+    result = multiplymatrices(matA, matA, matArows, matAcols, matArows, matAcols);
+    resultorder[0] = matArows;
+    resultorder[1] = matAcols;
+  }
+
+  if (optype == "SquareB")
+  {
+    result = multiplymatrices(matB, matB, matBrows, matBcols, matBrows, matBcols);
+    resultorder[0] = matBrows;
+    resultorder[1] = matBcols;
+  }
+
+  if (optype == "multconstA")
+  {
+    result = multiplybyconstant(matA, matArows, matAcols, constant);
+    resultorder[0] = matArows;
+    resultorder[1] = matAcols;
+  }
+
+  if (optype == "multconstB")
+  {
+    result = multiplybyconstant(matB, matBrows, matBcols, constant);
+    resultorder[0] = matBrows;
+    resultorder[1] = matBcols;
+  }
+
+  if (resultismatrix)
+  {
+    outputstring = ConvertMatToString(result, resultorder);
+  }
   //cout << outputstring;
   return outputstring;
 }
